3744063a10af3a874bc361dcf51621ce545b4026
So7eN7 <124157136+So7eN7@users.noreply.github.com>
2023-07-26 17:06:52 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import string
 import random
+import json
 
 # Simple password generator
 
@@ -30,16 +31,49 @@ def save_password_data():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     # Making sure that no fields are empty. If empty show the messagebox
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
-        messagebox.showinfo(title="OOPS", message="Please make sure to not leave any empty fields.")
+        messagebox.showinfo(title="Error", message="Please make sure to not leave any empty fields.")
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These details are entered: \n Email: {email}\n Password: {password}\n are they correct?")
         if is_ok: # If the fields are filled properly save them into the file
-            with open("Password Data.txt", "a") as data_file:
-                data_file.write(f"{website}, | {email} | {password}\n") # Writing into the file
-                # password_entry.delete(0, END)
+            try: # If the json file doesn't exist we need to create it
+                with open("Password Data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError: # Creating the json file
+                with open("Password Data.json", "w") as data_file:
+                    json.dump(data, data_file, indent=4)
+            else: # Updating the json file
+                data.update(new_data)
+                with open("Password Data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+
+# Search button function, finding passwords related to the website you're looking for
+
+
+def search_password():
+    website = website_entry.get()
+    try: # Loading the data
+        with open("Password Data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError: # Some basic error checking
+        messagebox.showinfo(title="Error", message="No data file found")
+    else: # Finding data if it's available
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}\n")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
 
 # GUI/main
 
@@ -70,6 +104,8 @@ email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 # Buttons
+search_button = Button(text="Search", width=15, command=search_password)
+search_button.grid(row=1, column=2)
 generate_password = Button(text="Generate Password", command=password_generator)
 generate_password.grid(row=3, column=2)
 add_button = Button(text="Add", width=30, command=save_password_data)
